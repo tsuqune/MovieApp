a320f4d671b27442f5bc9c09129f0274ad173154
revqueenge <Dealul02@gmail.com>
2025-04-26 18:17:02 +0700

    Создаем Retrofit-клиент

---
+++ app/src/main/java/com/bignerdranch/android/movieapp/api/RetrofitClient.kt
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.movieapp.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+    private const val API_KEY = "ВАШ_API_KEY" // Замените на свой!
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val tmdbApi: TmdbApi = retrofit.create(TmdbApi::class.java)
+}
